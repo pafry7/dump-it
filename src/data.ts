7218c3fbce8db2d37f17e003a36fc4645599589c
Kacper Żółkiewski <kacperzolkiewski@gmail.com>
2022-11-19 23:00:31 +0100

    create new data

--- src/data.ts
+++ src/data.ts
@@ -8,8 +8,49 @@ export interface Item {
 export const items: Item[] = [
   {
     name: "banana",
-    tips: ["Uzyj jako pojemnik", "Uzyj jako pojemnik", "Uzyj jako pojemnik"],
+    tips: [
+      "Nawóz pod inne rośliny",
+      "Sprawdzony krem nawilżający",
+      "Chcesz mieć bielsze zęby? Pocieraj je skórką od banana",
+    ],
     shareable: true,
     trashColor: "red",
   },
+  {
+    name: "plastic bottle",
+    tips: [
+      "Pojemnik do kiszenia ogórków",
+      "Karmnik dla ptaków",
+      "Doniczka na kwiaty",
+      "Wazon",
+      "Pojemnik na produkty sypkie",
+      "Szufelka na psie kupy",
+    ],
+    shareable: true,
+    trashColor: "yellow",
+  },
+  {
+    name: "glass bottle",
+    tips: [
+      "Minimalistyczny wazon na kwiaty",
+      "Własny dozownik do mydła",
+      "Lampki nocne z butelek",
+      "Wazon na kwiaty",
+    ],
+    shareable: true,
+    trashColor: "green",
+  },
+  {
+    name: "mobile device",
+    tips: [],
+    shareable: true,
+    trashColor: "green",
+  },
+];
+
+const electronicTips = [
+  "Zostaw zużyty sprzęt w sklepie, w którym kupujesz nowe urządzenie",
+  "Zostaw małogabarytowy zużyty sprzęt w dużym markecie bez konieczności kupowania nowego",
+  "Oddaj zużyty sprzęt elektryczny i elektroniczny w miejscu dostawy",
+  "Odnieś zużyty sprzęt do punktu zbierania",
 ];
